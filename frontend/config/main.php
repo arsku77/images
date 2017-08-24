f85ae274300499d62b8b4f7c580770ed3997f100
Arunas Skucas <arsku77@gmail.com>
2017-08-24 05:37:18 +0000

    urok22 improved user cabinet

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -45,7 +45,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                'profile/<nickname:\w+>' => 'user/profile/generate',
+                'profile/<nickname:\w+>' => 'user/profile/view',
             ],
         ],
     ],
--- frontend/modules/user/controllers/ProfileController.php
+++ frontend/modules/user/controllers/ProfileController.php
@@ -31,23 +31,23 @@ class ProfileController extends Controller
     }
 
 
-    public function actionGenerate()
-    {
-        $faker = \Faker\Factory::create();
-
-        for ($i = 0; $i < 1000; $i++) {
-            $user = new User([
-                'username' => $faker->name,
-                'email' => $faker->email,
-                'about' => $faker->text(200),
-                'nickname' => $faker->regexify('[A-Za-z0-9_]{5,15}'),
-                'auth_key' => Yii::$app->security->generateRandomString(),
-                'password_hash' => Yii::$app->security->generateRandomString(),
-                'created_at' => $time = time(),
-                'updated_at' => $time,
-            ]);
-            $user->save(false);
-        }
-    }
+//    public function actionGenerate()
+//    {
+//        $faker = \Faker\Factory::create();
+//
+//        for ($i = 0; $i < 1000; $i++) {
+//            $user = new User([
+//                'username' => $faker->name,
+//                'email' => $faker->email,
+//                'about' => $faker->text(200),
+//                'nickname' => $faker->regexify('[A-Za-z0-9_]{5,15}'),
+//                'auth_key' => Yii::$app->security->generateRandomString(),
+//                'password_hash' => Yii::$app->security->generateRandomString(),
+//                'created_at' => $time = time(),
+//                'updated_at' => $time,
+//            ]);
+//            $user->save(false);
+//        }
+//    }
 
 }
