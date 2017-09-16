96230554fc8eee908aa5d297701f343861f1e038
Arunas Skucas <arsku77@gmail.com>
2017-09-16 07:39:03 +0000

    urok24 addet post autor

--- frontend/models/Post.php
+++ frontend/models/Post.php
@@ -41,5 +41,13 @@ class Post extends \yii\db\ActiveRecord
     {
         return Yii::$app->storage->getFile($this->filename);
     }
+    /**
+     * Get author of the post
+     * @return User|null
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
 
 }
--- frontend/modules/post/views/default/view.php
+++ frontend/modules/post/views/default/view.php
@@ -11,6 +11,12 @@ use yii\web\JqueryAsset;
     <div class="row">
 
         <div class="col-md-12">
+            <?php if ($post->user): ?>
+                <?php echo $post->user->username; ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="col-md-12">
             <img src="<?php echo $post->getImage(); ?>" />
         </div>
 
