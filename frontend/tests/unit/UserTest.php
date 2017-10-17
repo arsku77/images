692e2ce4adae836b8f7f5aac5849ed43744a1883
Arunas Skucas <arsku77@gmail.com>
2017-10-17 19:33:23 +0000

    urok27 testet getNickName

---
+++ frontend/tests/unit/UserTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace frontend\tests;
+
+use frontend\tests\fixtures\UserFixture;
+
+class UserTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+
+    public function _fixtures()
+    {
+        return ['users' => UserFixture::className()];
+    }
+
+
+//    protected function _before()
+//    {
+//    }
+//
+//    protected function _after()
+//    {
+//    }
+
+//    // tests
+//    public function testSomeFeature()
+//    {
+//        $this->example();
+//    }
+    // next tests
+    public function testGetNickNameOnNicknameEmpty()
+    {
+    $user = $this->tester->grabFixture('users','user1');
+    expect($user->getNickname())->equals(1);
+    }
+
+    public function testGetNickNameOnNicknameNotEmpty()
+    {
+    $user = $this->tester->grabFixture('users','user2');
+    expect($user->getNickname())->equals('catelyn');
+    }
+
+//    public function example()
+//    {
+//
+//    }
+}
