03022807fd05b56713a95dd9bb1f110433ae5ad7
Arunas Skucas <arsku77@gmail.com>
2017-10-20 19:26:45 +0000

    urok27 test rule email unique

--- frontend/tests/unit/models/SignupFormTest.php
+++ frontend/tests/unit/models/SignupFormTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace frontend\tests\models;
 use frontend\modules\user\models\SignupForm;
-
+use frontend\tests\fixtures\UserFixture;
 
 
 class SignupFormTest extends \Codeception\Test\Unit
@@ -10,6 +10,16 @@ class SignupFormTest extends \Codeception\Test\Unit
      * @var \frontend\tests\UnitTester
      */
     protected $tester;
+
+    public function _before()
+    {
+        $this->tester->haveFixtures([
+            'user' => [
+                'class' => UserFixture::className(),
+            ]
+        ]);
+    }
+
 
     public function testTrimUsername()
     {
@@ -67,5 +77,22 @@ class SignupFormTest extends \Codeception\Test\Unit
         expect($model->getFirstError('password'))
             ->equals('Password cannot be blank.');
     }
+
+    public function testEmailUnique()
+    {
+        $model = new SignupForm([
+            'username' => 'some_username',
+            'email' => '1@got.com',
+            'password' => 'some_password',
+        ]);
+
+        $model->signup();
+
+        sleep(10);
+
+        expect($model->getFirstError('email'))
+            ->equals('This email address has already been taken.');
+    }
+
 
 }
