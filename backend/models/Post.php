efa3e6f502d5558cfae55bbbeab61fbf9d0235fb
Arunas Skucas <arsku77@gmail.com>
2017-11-01 14:47:59 +0000

    urok28 make CRUD for Post

---
+++ backend/models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $filename
+ * @property string $description
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $complaints
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'filename' => 'Filename',
+            'description' => 'Description',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'complaints' => 'Complaints',
+        ];
+    }
+}
