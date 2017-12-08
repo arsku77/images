49a0f3346f2ed7b39d71122e2e88d1a8989dc98b
Arunas Skucas <arsku77@gmail.com>
2017-12-08 09:53:57 +0000

    not bad2

--- frontend/modules/post/views/default/create.php
+++ frontend/modules/post/views/default/create.php
@@ -5,17 +5,27 @@
 use Yii;
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
+
+
 $this->title = Yii::t('post','Create');
 ?>
 
-<div class="post-default-index">
+<div class="page-posts no-padding">
+    <div class="row">
 
-    <h3><?php echo Yii::t('post','Create post'); ?></h3>
+        <div class="page page-post col-sm-12 col-xs-12 post-82">
+            <div class="row profile-posts">
 
-    <?php $form = ActiveForm::begin(); ?>
 
-    <?php echo $form->field($model, 'picture')->fileInput()
-        ->label(Yii::t('post','upload picture < 2mb')); ?>
+
+
+                <h3><?php echo Yii::t('post','Create post'); ?></h3>
+
+                <?php $form = ActiveForm::begin(); ?>
+
+    <?php echo $form->field($model, 'picture')
+        ->fileInput(['class' => 'btn btn-success',])
+        ->label(Yii::t('post','upload picture < 2mb'),['class'=>'btn btn-success']); ?>
 
     <?= $form->field($model, 'description')
         ->textarea(['rows' => 4,
@@ -26,8 +36,11 @@ $this->title = Yii::t('post','Create');
             'style' => 'font-weight:100;padding:0px 0px 0px 0px;margin: 10px 0px 0px 0px;width:70%',
         ]) ?>
 
-    <?php echo Html::submitButton('Create'); ?>
+                <?php echo Html::submitButton('Create', ['class' => 'btn btn-success']); ?>
 
-    <?php ActiveForm::end(); ?>
+                <?php ActiveForm::end(); ?>
 
+            </div>
+        </div>
+    </div>
 </div>
